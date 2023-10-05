2ca4d5f1e8a141ceb7a4271c46f26320310345ff
Gernot H. Reichel <gernot.reichel@me.com>
2023-10-05 13:25:16 +0200

    corrected debug log #3

--- repo_github-event-handler.js
+++ repo_github-event-handler.js
@@ -1,9 +1,9 @@
 /**
  * Callback for Webhook
  * @param {*} req Request Object (to be parsed)
- * @param {*} res Response Object
+ * @param {*} res Response Object (status to be set)
  */
 exports.webhook = (req, res) => {
-   console.table(JSON.stringify(req.body.check_run));
+   console.log(JSON.stringify(req.body.check_run), req.body.action);
    res.status(200).send('ok');
 };
