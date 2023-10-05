44c8ead555289d0b333e1154fd1a529350c223e4
Gernot H. Reichel <gernot.reichel@me.com>
2023-10-05 13:19:39 +0200

    test push #6

--- repo_github-event-handler.js
+++ repo_github-event-handler.js
@@ -1,3 +1,8 @@
+/**
+ * Callback for Webhook
+ * @param {*} req
+ * @param {*} res
+ */
 exports.webhook = (req, res) => {
    console.table(req.body.check_run.ii, req.body.check_run.name, req.body.check_run.conclusion);
    res.status(200).send('ok');
