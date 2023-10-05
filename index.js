07bf21252e125389630da4c9ee31a628f27d3d47
Gernot H. Reichel <gernot.reichel@me.com>
2023-10-05 09:39:16 +0200

    implementing github webhook secret

--- index.js
+++ index.js
@@ -1,32 +1,64 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const repoGithubEventHandler = require('./repo_github-event-handler');
+const secret = process.env.GITHUB_WEBHOOK_SECRET;
+
+// .......................................................................
+// For these headers, a sigHashAlg of sha1 must be used instead of sha256
+// GitHub: X-Hub-Signature
+// Gogs:   X-Gogs-Signature
+// .......................................................................
+const sigHeaderName = 'X-Hub-Signature-256'
+const sigHashAlg = 'sha256'
 
 app.set('host', process.env.IP || '0.0.0.0');
 app.set('port', process.env.PORT || '31000');
 app.use(express.json());
 
 // .......................................................................
+// Saves a valid raw JSON body to req.rawBody
+// Credits to https://stackoverflow.com/a/35651853/90674
+// .......................................................................
+app.use(bodyParser.json({
+   verify: (req, res, buf, encoding) => {
+      if (buf && buf.length) {
+         req.rawBody = buf.toString(encoding || 'utf8');
+      }
+   },
+}));
+
+function verifyPostData(req, res, next) {
+   if (!req.rawBody) {
+      return next('Request body empty')
+   }
+
+   const sig = Buffer.from(req.get(sigHeaderName) || '', 'utf8')
+   const hmac = crypto.createHmac(sigHashAlg, secret)
+   const digest = Buffer.from(sigHashAlg + '=' + hmac.update(req.rawBody).digest('hex'), 'utf8')
+   if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
+      return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`)
+   }
+
+   return next()
+};
+
+// .......................................................................
 // Endpoints
 // .......................................................................
 console.log('listening on:');
-
-app.post('/github/webhook/githubEventHandler', repoGithubEventHandler.webhook);
+app.post('/github/webhook/githubEventHandler', verifyPostData, repoGithubEventHandler.webhook);
 console.log('/github/webhook/githubEventHandler')
-console.log(process.env.GITHUB_WEBHOOK_SECRET);
 
 // .......................................................................
 // Optional fallthrough error handler
 // .......................................................................
 app.use((err, req, res, next) => {
-   // The error id is attached to `res.sentry` to be returned
-   // and optionally displayed to the user for support.
-   res.statusCode = 500;
-   res.end(`${res}\n`);
-   // res.end(res.sentry + "\n");
-});
+   if (err) console.error(err)
+   res.status(403).send('Request body was not signed or verification failed')
+})
 
 /* ================= start the web service on http ================= */
 const httpServer = http.createServer(app).listen(app.get('port'), app.get('host'), () => {
