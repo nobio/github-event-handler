3050e65982bc8d9700cf859223571f2d440a1316
Gernot H. Reichel <gernot.reichel@me.com>
2023-10-05 14:16:03 +0200

    corrected debug log #7

--- repo_github-event-handler.js
+++ repo_github-event-handler.js
@@ -4,6 +4,6 @@
  * @param {*} res Response Object (status to be set)
  */
 exports.webhook = (req, res) => {
-   console.log(JSON.stringify(req.body.check_run.name) + " - " + req.body.action);
+   console.log(JSON.stringify(req.body.check_run));
    res.status(200).send('okidokijou');
 };
