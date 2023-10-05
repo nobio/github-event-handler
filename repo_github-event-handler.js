11145b620d8099fbdc04cc0a5971641298598584
Gernot H. Reichel <gernot.reichel@me.com>
2023-10-05 13:20:20 +0200

    corrected debug log

--- repo_github-event-handler.js
+++ repo_github-event-handler.js
@@ -4,6 +4,6 @@
  * @param {*} res
  */
 exports.webhook = (req, res) => {
-   console.table(req.body.check_run.ii, req.body.check_run.name, req.body.check_run.conclusion);
+   console.table(req.body.check_run.id, req.body.check_run.name, req.body.check_run.conclusion);
    res.status(200).send('ok');
 };
