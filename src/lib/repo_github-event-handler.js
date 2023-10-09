548b7dc10cca5e48db1dc64e985f2f79daae8c31
Gernot H. Reichel <gernot.reichel@me.com>
2023-10-09 12:42:17 +0200

    refactoring

---
+++ src/lib/repo_github-event-handler.js
@@ -0,0 +1,57 @@
+const {
+   GITHUB_WEBHOOK_TRIGGER_TOUCH_PATH,
+   GITHUB_WEBHOOK_TRIGGER
+} = process.env;
+
+const util = require('./util');
+const triggerConditions = GITHUB_WEBHOOK_TRIGGER.split(', ');
+
+//console.log(`Trigger condition: "${GITHUB_WEBHOOK_TRIGGER_NAME} ${GITHUB_WEBHOOK_TRIGGER_ACTION}"`);
+console.log(`Trigger conditions: ${triggerConditions}`);
+
+/**
+ * Callback for webhook
+ *
+ * curl -X POST -H "Content-Type: application/json" -d @./src/ressources/webkook.json -i http://localhost:31000/github/webhook/githubEventHandler
+ *
+ * @param {*} req Request Object (to be parsed)
+ * @param {*} res Response Object (status to be set)
+ */
+exports.webhook = (req, res) => {
+   if (req.body.check_run) {
+      //if (req.body.check_run.name == GITHUB_WEBHOOK_TRIGGER_NAME && req.body.action == GITHUB_WEBHOOK_TRIGGER_ACTION) {
+      const triggerCondition = hasTriggered(req, triggerConditions)
+      if (triggerCondition) {
+         console.log(`TRIGGERED!!! ' + ${JSON.stringify(triggerCondition)}`);
+         util.execCmd('touch', [`${GITHUB_WEBHOOK_TRIGGER_TOUCH_PATH}/deply.${triggerCondition.repo}`]);
+
+      } else {
+         console.log(`ignoring ${JSON.stringify(req.body.check_run.name)} ${req.body.action}`);
+      }
+   }
+   res.status(200).send();
+};
+
+/**
+ *
+ * @param {*} req
+ * @param {*} triggerCondition
+ * @returns
+ */
+function hasTriggered(req, triggerCondition) {
+   const repo = req.body.repository.name
+   const name = req.body.check_run.name
+   const status = req.body.action
+
+   const trigger = `${repo}.${name}.${status}`
+
+   if (triggerCondition.includes(trigger)) {
+      return {
+         repo,
+         name,
+         status
+      };
+   } else {
+      return undefined;
+   }
+}
---
+++ src/lib/util.js
@@ -0,0 +1,40 @@
+const { GITHUB_WEBHOOK_TRIGGER_CMD, GITHUB_WEBHOOK_TRIGGER_CMD_ARGS } = process.env;
+const { spawn } = require("child_process");
+
+// console.log(GITHUB_WEBHOOK_TRIGGER_CMD);
+// console.log(GITHUB_WEBHOOK_TRIGGER_CMD_ARGS);
+// console.log(GITHUB_WEBHOOK_TRIGGER_CMD_ARGS.split(' '));
+
+exports.execCmd = async (command, args) => {
+   console.log(' execute command\n   "' + command + '" with arguemts: "' + args + '"');
+
+   // prepare the command
+   let cmd;
+
+   if (!args) {
+      cmd = spawn(command);
+   } else {
+      cmd = spawn(command, args);
+   }
+
+   cmd.stdout.on("data", data => {
+      console.log(`stdout: ${data}`);
+   });
+
+   cmd.stderr.on("data", data => {
+      console.log(`stderr: ${data}`);
+   });
+
+   cmd.on('error', (error) => {
+      console.log(`error: ${error.message}`);
+   });
+
+   cmd.on("close", code => {
+      console.log(`child process exited with code ${code}`);
+   });
+
+};
+
+exports.experiment = (req, res) => {
+   res.status(200).send('');
+};
