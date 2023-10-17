8fef1b7f7f97a8fc2de9d6c6371416af72566713
Gernot H. Reichel <gernot.reichel@me.com>
2023-10-17 15:19:26 +0200

    fixed execute script

--- src/ressources/execute-github-webhook.sh
+++ src/ressources/execute-github-webhook.sh
@@ -1,6 +1,3 @@
-# ====================================================
-# Auth: Gernot Reichel
-# ====================================================
 HOME=/home/pi/container
 GHWH_HOME=$HOME/github-event-handler
 DOCKER_COMPOSE_HOME=/usr/local/bin
@@ -10,29 +7,30 @@ DEBUGFILE=$HOME/github-webhook.log
 
 for deployment in $DEPLOYMENTS
 do
-  echo ================= start deployment for $deployment ================= >> $DEBUGFILE
+  repo=$(echo ${deployment} | awk -Fdeploy. '{print $2}')
+  echo -e "\r" >> $DEBUGFILE
+  echo $(date +%F_%X) ================= start deployment for $repo ================= >> $DEBUGFILE
 
-  repo_dir=$(echo ${deployment} | awk -F. '{print $2}')
+  #cmdPre=$(echo $(which docker-compose) -f ${HOME}/${repo}/docker-compose.yaml )
+  cmdPre=$(echo ${DOCKER_COMPOSE_HOME}/docker-compose -f ${HOME}/${repo}/docker-compose.yaml )
 
-  #cmdPre=$(echo $(which docker-compose) -f ${HOME}/${repo_dir}/docker-compose.yaml )
-  cmdPre=$(echo ${DOCKER_COMPOSE_HOME}/docker-compose -f ${HOME}/${repo_dir}/docker-compose.yaml )
+  echo $(date +%F_%X) $cmdPre >> $DEBUGFILE
 
-  echo $cmdPre >> $DEBUGFILE
-
-  echo '  > ' $cmdPre pull  >> $DEBUGFILE
+  echo $(date +%F_%X) '  > ' $cmdPre pull  >> $DEBUGFILE
   eval $cmdPre pull  >> $DEBUGFILE
 
-  echo '  > ' $cmdPre down  >> $DEBUGFILE
+  echo $(date +%F_%X ) '  > ' $cmdPre down  >> $DEBUGFILE
   eval $cmdPre down  >> $DEBUGFILE
 
-  echo '  > ' $cmdPre up -d  >> $DEBUGFILE
+  echo $(date +%F_%X) '  > ' $cmdPre up -d  >> $DEBUGFILE
   eval $cmdPre up -d  >> $DEBUGFILE
 
-  echo '  > ' docker system prune -a -f >> $DEBUGFILE
+  echo $(date +%F_%X) '  > ' docker system prune -a -f >> $DEBUGFILE
   eval docker system prune -a -f >> $DEBUGFILE
-  echo '  > ' docker volume prune -f >> $DEBUGFILE
+  echo $(date +%F_%X) '  > ' docker volume prune -f >> $DEBUGFILE
   eval docker volume prune -f >> $DEBUGFILE
 
-  echo '  > ' rm $deployment >> $DEBUGFILE
+  echo $(date +%F_%X) '  > ' rm $deployment >> $DEBUGFILE
   rm -f $deployment
+  echo $(date +%F_%X) ================= end of deployment for $repo ================= >> $DEBUGFILE
 done
